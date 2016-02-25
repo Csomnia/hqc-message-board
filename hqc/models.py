35b10e1cbe270695ab48b7369b38c4b869515c3a
csomnia <changrunqing@gmail.com>
2016-02-26 00:04:19 +0800

    add email validator, fix id_incrament.

--- hqc/models.py
+++ hqc/models.py
@@ -1,27 +1,28 @@
 import datetime
 from flask import url_for
 from hqc import db
+from wtforms.validators import Email
 
 
-class Reply(db.Document):
+class Reply(db.EmbeddedDocument):
     """define doucument of replies
 
     """
-    author = db.StringField(max_length=255, request=True)
-    created_at = db.DateTimeField(default=datetime.datetime.now, require=True)
+    author = db.StringField(max_length=255, required=True)
+    created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
     body = db.StringField(require=True)
 
 
-class Message(db.Document):
+class Message(db.DynamicDocument):
     """define document of messages
 
     """
     msg_id = db.IntField(require=True)
-    created_at = db.DateTimeField(default=datetime.datetime.now, require=True)
-    title = db.StringField(max_length=255, require=True)
-    email = db.StringField(max_length=255, require=True)
-    is_show = db.BooleanField(default=False)
-    body = db.StringField(require=True)
+    created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
+    title = db.StringField(max_length=255, required=True)
+    email = db.StringField(max_length=255, required=True, validators=[Email()])
+    is_show = db.BooleanField(default=True)
+    body = db.StringField(required=True)
     tags = db.ListField(db.StringField(max_length=50))
     replies = db.ListField(db.EmbeddedDocumentField('Reply'))
 
